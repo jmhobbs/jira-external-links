0b0c5d550a484b3800c2bcca0d43c382c5a55af6
John Hobbs <john@velvetcache.org>
2017-05-04 15:33:12 -0500

    Fix PR links.

--- JIRA-External-Links/content.js
+++ JIRA-External-Links/content.js
@@ -1,5 +1,5 @@
 document.addEventListener('click', function(event) {
-  if(event.target.classList.contains("external-link")) {
+  if(event.target.classList.contains("external-link") || event.target.classList.contains("pullrequest-link")) {
     event.preventDefault();
     chrome.runtime.sendMessage({url: event.target.href}, function () {});
   }
