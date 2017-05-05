0ac1f2e972974e1bdc0a696e866ba0d174985465
John Hobbs <john@velvetcache.org>
2017-05-05 11:29:04 -0500

    If the extension reloads, don't stifle links.

--- JIRA-External-Links/content.js
+++ JIRA-External-Links/content.js
@@ -1,6 +1,11 @@
 document.addEventListener('click', function(event) {
   if(event.target.classList.contains("external-link") || event.target.classList.contains("pullrequest-link")) {
-    event.preventDefault();
-    chrome.runtime.sendMessage({url: event.target.href}, function () {});
+    try {
+      chrome.runtime.sendMessage({url: event.target.href}, function () {});
+      event.preventDefault();
+    }
+    catch(err) {
+      console.log(err);
+    }
   }
 });
